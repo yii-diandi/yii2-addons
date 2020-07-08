3a31575d8eda90590a43653dd829fa4927f1cac5
途火科技官微 <2192138785@qq.com>
2020-07-08 22:17:52 +0800

    wer

--- loader.php
+++ loader.php
@@ -4,7 +4,7 @@
  * @Author: Wang Chunsheng 2192138785@qq.com
  * @Date:   2020-03-26 12:59:45
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2020-07-02 13:52:05
+ * @Last Modified time: 2020-07-08 21:27:56
  */
 
 namespace diandi\addons;
@@ -32,8 +32,7 @@ class Loader implements BootstrapInterface
      */
     public function bootstrap($application)
     {
-        global $_W,$_GPC;
-        $_W = Yii::$app->params;
+        global $_GPC;
         $this->id = Yii::$app->id; 
         if(Yii::$app->id=='app-console'){
             $this->afreshLoad('','','');
