7b68fe93f8fceede88fdaf7653cf66e6b4904095
chunchun <2192138785@qq.com>
2021-12-27 08:26:52 +0800

    2.57

--- models/form/Oss.php
+++ models/form/Oss.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-04-30 17:03:38
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2021-12-26 19:46:57
+ * @Last Modified time: 2021-12-27 08:24:25
  */
 
 namespace diandi\addons\models\form;
@@ -19,7 +19,7 @@ class Oss extends Model
      */
     public $id;
     public $bloc_id;
-    
+
     public $Aliyunoss_accessKeyId;
     public $Aliyunoss_resource;
     public $Aliyunoss_accessKeySecret;
@@ -35,8 +35,7 @@ class Oss extends Model
     public $Qiniuoss_Bucket;
     public $Qiniuoss_url;
     public $remote_type;
-    public $Aliyunoss_bucket;
-    
+
     /**
      * {@inheritdoc}
      */
@@ -44,7 +43,7 @@ class Oss extends Model
     {
         return [
             [['Aliyunoss_accessKeyId',
-            'Aliyunoss_bucket',
+            'Aliyunoss_resource',
             'Aliyunoss_accessKeySecret',
             'Aliyunoss_url',
             'Tengxunoss_APPID',
@@ -57,8 +56,8 @@ class Oss extends Model
             'Qiniuoss_Secretkey',
             'Qiniuoss_Bucket',
             'remote_type',
-            'Qiniuoss_url'], 'string'],
-            [['id', 'bloc_id','Aliyunoss_resource'], 'integer'],
+            'Qiniuoss_url', ], 'string'],
+            [['id', 'bloc_id'], 'integer'],
         ];
     }
 
@@ -67,27 +66,24 @@ class Oss extends Model
         $conf = new BlocConfOss();
         $bloc = $conf::find()->where(['bloc_id' => $bloc_id])->asArray()->one();
 
-
         $this->id = $bloc['id'];
         $this->bloc_id = $bloc['bloc_id'];
-        
-        $this->Aliyunoss_bucket=$bloc['Aliyunoss_bucket'];
-        $this->Aliyunoss_accessKeyId=$bloc['remote_type'];
-        $this->Aliyunoss_accessKeyId=$bloc['Aliyunoss_accessKeyId'];
-        $this->Aliyunoss_resource=$bloc['Aliyunoss_resource'];
-        $this->Aliyunoss_accessKeySecret=$bloc['Aliyunoss_accessKeySecret'];
-        $this->Aliyunoss_url=$bloc['Aliyunoss_url'];
-        $this->Tengxunoss_APPID=$bloc['Tengxunoss_APPID'];
-        $this->Tengxunoss_SecretID=$bloc['Tengxunoss_SecretID'];
-        $this->Tengxunoss_SecretKEY=$bloc['Tengxunoss_SecretKEY'];
-        $this->Tengxunoss_Bucket=$bloc['Tengxunoss_Bucket'];
-        $this->Tengxunoss_area=$bloc['Tengxunoss_area'];
-        $this->Tengxunoss_url=$bloc['Tengxunoss_url'];
-        $this->Qiniuoss_Accesskey=$bloc['Qiniuoss_Accesskey'];
-        $this->Qiniuoss_Secretkey=$bloc['Qiniuoss_Secretkey'];
-        $this->Qiniuoss_Bucket=$bloc['Qiniuoss_Bucket'];
-        $this->Qiniuoss_url=$bloc['Qiniuoss_url'];
-        
+
+        $this->remote_type = $bloc['remote_type'];
+        $this->Aliyunoss_accessKeyId = $bloc['Aliyunoss_accessKeyId'];
+        $this->Aliyunoss_resource = $bloc['Aliyunoss_resource'];
+        $this->Aliyunoss_accessKeySecret = $bloc['Aliyunoss_accessKeySecret'];
+        $this->Aliyunoss_url = $bloc['Aliyunoss_url'];
+        $this->Tengxunoss_APPID = $bloc['Tengxunoss_APPID'];
+        $this->Tengxunoss_SecretID = $bloc['Tengxunoss_SecretID'];
+        $this->Tengxunoss_SecretKEY = $bloc['Tengxunoss_SecretKEY'];
+        $this->Tengxunoss_Bucket = $bloc['Tengxunoss_Bucket'];
+        $this->Tengxunoss_area = $bloc['Tengxunoss_area'];
+        $this->Tengxunoss_url = $bloc['Tengxunoss_url'];
+        $this->Qiniuoss_Accesskey = $bloc['Qiniuoss_Accesskey'];
+        $this->Qiniuoss_Secretkey = $bloc['Qiniuoss_Secretkey'];
+        $this->Qiniuoss_Bucket = $bloc['Qiniuoss_Bucket'];
+        $this->Qiniuoss_url = $bloc['Qiniuoss_url'];
     }
 
     public function saveConf($bloc_id)
@@ -95,32 +91,29 @@ class Oss extends Model
         if (!$this->validate()) {
             return null;
         }
-        
+
         $conf = BlocConfOss::findOne(['bloc_id' => $bloc_id]);
-        
+
         if (!$conf) {
             $conf = new BlocConfOss();
         }
-        
-        $conf->bloc_id = $bloc_id;
-        
-        $conf->Aliyunoss_bucket = $this->Aliyunoss_bucket;
-        $conf->remote_type = $this->remote_type;
-        $conf->Aliyunoss_accessKeyId= $this->Aliyunoss_accessKeyId;
-        $conf->Aliyunoss_resource= $this->Aliyunoss_resource;
-        $conf->Aliyunoss_accessKeySecret= $this->Aliyunoss_accessKeySecret;
-        $conf->Aliyunoss_url= $this->Aliyunoss_url;
-        $conf->Tengxunoss_APPID= $this->Tengxunoss_APPID;
-        $conf->Tengxunoss_SecretID= $this->Tengxunoss_SecretID;
-        $conf->Tengxunoss_SecretKEY= $this->Tengxunoss_SecretKEY;
-        $conf->Tengxunoss_Bucket= $this->Tengxunoss_Bucket;
-        $conf->Tengxunoss_area= $this->Tengxunoss_area;
-        $conf->Tengxunoss_url= $this->Tengxunoss_url;
-        $conf->Qiniuoss_Accesskey= $this->Qiniuoss_Accesskey;
-        $conf->Qiniuoss_Secretkey= $this->Qiniuoss_Secretkey;
-        $conf->Qiniuoss_Bucket= $this->Qiniuoss_Bucket;
-        $conf->Qiniuoss_url= $this->Qiniuoss_url;
 
+        $conf->bloc_id = $bloc_id;
+        $conf->remote_type = $this->remote_type;
+        $conf->Aliyunoss_accessKeyId = $this->Aliyunoss_accessKeyId;
+        $conf->Aliyunoss_resource = $this->Aliyunoss_resource;
+        $conf->Aliyunoss_accessKeySecret = $this->Aliyunoss_accessKeySecret;
+        $conf->Aliyunoss_url = $this->Aliyunoss_url;
+        $conf->Tengxunoss_APPID = $this->Tengxunoss_APPID;
+        $conf->Tengxunoss_SecretID = $this->Tengxunoss_SecretID;
+        $conf->Tengxunoss_SecretKEY = $this->Tengxunoss_SecretKEY;
+        $conf->Tengxunoss_Bucket = $this->Tengxunoss_Bucket;
+        $conf->Tengxunoss_area = $this->Tengxunoss_area;
+        $conf->Tengxunoss_url = $this->Tengxunoss_url;
+        $conf->Qiniuoss_Accesskey = $this->Qiniuoss_Accesskey;
+        $conf->Qiniuoss_Secretkey = $this->Qiniuoss_Secretkey;
+        $conf->Qiniuoss_Bucket = $this->Qiniuoss_Bucket;
+        $conf->Qiniuoss_url = $this->Qiniuoss_url;
 
         if ($conf->save()) {
             return [
@@ -143,20 +136,20 @@ class Oss extends Model
     public function attributeLabels(): array
     {
         return [
-            'Aliyunoss_accessKeyId'=>'Aliyunoss_accessKeyId',
-            'Aliyunoss_resource'=>'Aliyunoss_resource',
-            'Aliyunoss_accessKeySecret'=>'Aliyunoss_accessKeySecret',
-            'Aliyunoss_url'=>'Aliyunoss_url',
-            'Tengxunoss_APPID'=>'Tengxunoss_APPID',
-            'Tengxunoss_SecretID'=>'Tengxunoss_SecretID',
-            'Tengxunoss_SecretKEY'=>'Tengxunoss_SecretKEY',
-            'Tengxunoss_Bucket'=>'Tengxunoss_Bucket',
-            'Tengxunoss_area'=>'Tengxunoss_area',
-            'Tengxunoss_url'=>'Tengxunoss_url',
-            'Qiniuoss_Accesskey'=>'Qiniuoss_Accesskey',
-            'Qiniuoss_Secretkey'=>'Qiniuoss_Secretkey',
-            'Qiniuoss_Bucket'=>'Qiniuoss_Bucket',
-            'Qiniuoss_url'=>'Qiniuoss_url'
+            'Aliyunoss_accessKeyId' => 'Aliyunoss_accessKeyId',
+            'Aliyunoss_resource' => 'Aliyunoss_resource',
+            'Aliyunoss_accessKeySecret' => 'Aliyunoss_accessKeySecret',
+            'Aliyunoss_url' => 'Aliyunoss_url',
+            'Tengxunoss_APPID' => 'Tengxunoss_APPID',
+            'Tengxunoss_SecretID' => 'Tengxunoss_SecretID',
+            'Tengxunoss_SecretKEY' => 'Tengxunoss_SecretKEY',
+            'Tengxunoss_Bucket' => 'Tengxunoss_Bucket',
+            'Tengxunoss_area' => 'Tengxunoss_area',
+            'Tengxunoss_url' => 'Tengxunoss_url',
+            'Qiniuoss_Accesskey' => 'Qiniuoss_Accesskey',
+            'Qiniuoss_Secretkey' => 'Qiniuoss_Secretkey',
+            'Qiniuoss_Bucket' => 'Qiniuoss_Bucket',
+            'Qiniuoss_url' => 'Qiniuoss_url',
         ];
     }
 }
