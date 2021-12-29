fffaa087ee4c7d2f060d43ab24cfc0a937888ca6
chunchun <2192138785@qq.com>
2021-12-29 10:27:04 +0800

    1.0.2

---
+++ addons/update.php
@@ -0,0 +1,13 @@
+<?php
+
+use yii\base\Controller;
+
+/**
+ * @Author: Wang Chunsheng 2192138785@qq.com
+ * @Date:   2020-03-26 13:04:44
+ * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
+ * @Last Modified time: 2021-12-28 17:01:49
+ */
+class update extends Controller
+{
+}
